8ff3d940ac70788c2de06422846da43d94a63719
sc-GiulianoFrau <giuliano.frau@smart-contact.it>
2024-09-17 12:48:50 +0200

    feat:cambiato icone

--- src/components/AppMenu.jsx
+++ src/components/AppMenu.jsx
@@ -2,9 +2,9 @@ import { Menubar } from "primereact/menubar";
 
 const AppMenu = () => {
   const items = [
-    { label: "Tutti i pokemon", icon: "pi pi-list", url: "/" },
-    { label: "Il mio pokedex", icon: "pi pi-users", url: "/pokedex" },
-    { label: "About", icon: "pi pi-user", url: "/about" },
+    { label: "Tutti i pokemon", icon: "pi pi-database", url: "/" },
+    { label: "Il mio pokedex", icon: "pi pi-save", url: "/pokedex" },
+    { label: "About", icon: "pi pi-github", url: "/about" },
   ];
 
   return <Menubar model={items} className=" shadow-md p-4 border-gray-100" />;
