4ad2b671f65a3aa2019906f45dbc437f8ab074fa
sc-GiulianoFrau <giuliano.frau@smart-contact.it>
2024-09-17 15:40:28 +0200

    refact code

--- src/components/PokemonDetail.jsx
+++ src/components/PokemonDetail.jsx
@@ -19,72 +19,78 @@ const AppPokemonDetails = () => {
   }, [id]);
 
   return (
-    <div className="app__details w-11/12 sm:w-2/3 md:w-1/2 lg:w-1/3 m-auto my-10 py-8 bg-white border border-gray-300 shadow-xl rounded-xl text-center text-xl">
-      <div className="text-3xl font-bold text-gray-900 mb-4 uppercase">
-        {pokemon?.name || "Nome non disponibile"}
-      </div>
+    <div className="app__details py-6">
+      <div className=" w-11/12 sm:w-2/3 md:w-1/2 lg:w-1/3 m-auto my-10 py-8 bg-white border border-gray-300 shadow-xl rounded-xl text-center text-xl">
+        <div className="text-3xl font-bold text-gray-900 mb-4 uppercase">
+          {pokemon?.name || "Nome non disponibile"}
+        </div>
 
-      <div className="flex flex-col justify-center gap-4 mb-6 items-center">
-        <img
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-          src={isHovered ? pokemonInfo?.sprites?.back_default || image : image}
-          alt={pokemon?.name}
-          className="w-40 sm:w-1/4 mx-auto transition-transform duration-300 ease-in-out transform hover:scale-110"
-        />
+        <div className="flex flex-col justify-center gap-4 mb-6 items-center">
+          <img
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            src={
+              isHovered ? pokemonInfo?.sprites?.back_default || image : image
+            }
+            alt={pokemon?.name}
+            className="w-40 sm:w-1/4 mx-auto transition-transform duration-300 ease-in-out transform hover:scale-110"
+          />
 
-        {pokemonInfo ? (
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 items-start w-full px-4">
-            <div className="bg-gray-50 rounded-lg p-4 shadow-sm border h-full">
-              <b className="text-gray-700">Pokemon ID</b> <br />
-              <span className="text-gray-600">{id}</span>
+          {pokemonInfo ? (
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 items-start w-full px-4">
+              <div className="bg-gray-50 rounded-lg p-4 shadow-sm border h-full">
+                <b className="text-gray-700">Pokemon ID</b> <br />
+                <span className="text-gray-600">{id}</span>
+              </div>
+              <div className="bg-gray-50 rounded-lg p-4 shadow-sm border h-full">
+                <b className="text-gray-700">Tipo</b> <br />
+                <span className="capitalize text-gray-600">
+                  {pokemonInfo.types.map((pokemonType, index) => (
+                    <span key={index}>
+                      {pokemonType.type.name}
+                      {index !== pokemonInfo.types.length - 1 && (
+                        <span>, </span>
+                      )}
+                    </span>
+                  ))}
+                </span>
+              </div>
+              <div className="bg-gray-50 rounded-lg p-4 shadow-sm border h-full">
+                <b className="text-gray-700">Abilità</b> <br />
+                <span className="capitalize text-gray-600">
+                  {pokemonInfo.abilities.map((pokemonAbility, index) => (
+                    <span key={index}>
+                      {pokemonAbility.ability.name}
+                      {index !== pokemonInfo.abilities.length - 1 && (
+                        <span>, </span>
+                      )}
+                    </span>
+                  ))}
+                </span>
+              </div>
+              <div className="bg-gray-50 rounded-lg p-4 shadow-sm border h-full">
+                <b className="text-gray-700">Peso</b> <br />
+                <span className="text-gray-600">{pokemonInfo.weight}</span>
+              </div>
             </div>
-            <div className="bg-gray-50 rounded-lg p-4 shadow-sm border h-full">
-              <b className="text-gray-700">Tipo</b> <br />
-              <span className="capitalize text-gray-600">
-                {pokemonInfo.types.map((pokemonType, index) => (
-                  <span key={index}>
-                    {pokemonType.type.name}
-                    {index !== pokemonInfo.types.length - 1 && <span>, </span>}
-                  </span>
-                ))}
-              </span>
-            </div>
-            <div className="bg-gray-50 rounded-lg p-4 shadow-sm border h-full">
-              <b className="text-gray-700">Abilità</b> <br />
-              <span className="capitalize text-gray-600">
-                {pokemonInfo.abilities.map((pokemonAbility, index) => (
-                  <span key={index}>
-                    {pokemonAbility.ability.name}
-                    {index !== pokemonInfo.abilities.length - 1 && (
-                      <span>, </span>
-                    )}
-                  </span>
-                ))}
-              </span>
-            </div>
-            <div className="bg-gray-50 rounded-lg p-4 shadow-sm border h-full">
-              <b className="text-gray-700">Peso</b> <br />
-              <span className="text-gray-600">{pokemonInfo.weight}</span>
-            </div>
-          </div>
-        ) : (
-          <div className="text-gray-500">Caricamento informazioni...</div>
-        )}
-      </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 items-start  px-4">
-        <Link
-          className="inline-block bg-teal-500 text-white hover:text-white rounded-md py-2 px-4 hover:bg-teal-700 transition-colors shadow-md"
-          to="/"
-        >
-          Torna alla Lista
-        </Link>
-        <Link
-          className="inline-block bg-yellow-500 text-white hover:text-white  rounded-md py-2 px-4 hover:bg-yellow-600 transition-colors shadow-md"
-          to="/pokedex"
-        >
-          Torna al Pokedex
-        </Link>
+          ) : (
+            <div className="text-gray-500">Caricamento informazioni...</div>
+          )}
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 items-start  px-4">
+          <Link
+            className="inline-block bg-teal-500 text-white hover:text-white rounded-md py-2 px-4 hover:bg-teal-700 transition-colors shadow-md"
+            to="/"
+          >
+            Torna alla Lista <i className="ml-3 pi pi-database"></i>
+          </Link>
+          <Link
+            className="inline-block bg-yellow-500 text-white hover:text-white  rounded-md py-2 px-4 hover:bg-yellow-600 transition-colors shadow-md"
+            to="/pokedex"
+          >
+            Torna al Pokedex <i className="ml-3 pi pi-save"></i>
+          </Link>
+        </div>
       </div>
     </div>
   );
