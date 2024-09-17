c358abb733db69cd6c4f2348950bb9f86b5909dd
sc-GiulianoFrau <giuliano.frau@smart-contact.it>
2024-09-17 11:02:39 +0200

    feat: ordinati project

--- src/components/AppProject.jsx
+++ src/components/AppProject.jsx
@@ -1,29 +1,12 @@
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-
-const project = [
-  {
-    progetto: "Crud application Paesi del mondo",
-    link_progetto: "https://react-crud-app-eosin.vercel.app/",
-    framework: "React 18",
-    tecnologie:
-      "React, Redux, Tailwind CSS, PrimeReact, Axios,Rest API, React Router, Vite",
-    link_repo: "https://github.com/giulianoFrau/react-approach.git",
-  },
-  {
-    progetto: "Sito vetrina",
-    link_progetto: "https://sitovetrina.vercel.app/#/homePage",
-    framework: "Vue 3",
-    tecnologie: "Vue, PrimeVue, Tailwind CSS, Pinia, Vue Router, Vite",
-    link_repo: "https://github.com/giulianoFrau/vetrina.git",
-  },
-];
+import { projects } from "../options/projects.js";
 
 const AppProject = () => {
   return (
     <div className="card">
       <DataTable
-        value={project}
+        value={projects}
         stripedRows
         tableStyle={{ minWidth: "50rem" }}
         className="p-0"
---
+++ src/options/projects.js
@@ -0,0 +1,17 @@
+export const projects = [
+  {
+    progetto: "Crud application Paesi del mondo",
+    link_progetto: "https://react-crud-app-eosin.vercel.app/",
+    framework: "React 18",
+    tecnologie:
+      "React, Redux, Tailwind CSS, PrimeReact, Axios,Rest API, React Router, Vite",
+    link_repo: "https://github.com/giulianoFrau/react-approach.git",
+  },
+  {
+    progetto: "Sito vetrina",
+    link_progetto: "https://sitovetrina.vercel.app/#/homePage",
+    framework: "Vue 3",
+    tecnologie: "Vue, PrimeVue, Tailwind CSS, Pinia, Vue Router, Vite",
+    link_repo: "https://github.com/giulianoFrau/vetrina.git",
+  },
+];
